2980b78dc8ebf84ffc7c849ca6af8e7efaf62f18
Akshay <107635864+CodeOfGodMaster@users.noreply.github.com>
2025-08-27 08:09:20 +0530

    Initial project commit

---
+++ ecommerce/custom_filters.py
@@ -0,0 +1,13 @@
+# custom_filters.py
+
+from django import template
+import json
+
+register = template.Library()
+
+@register.filter
+def parse_json(value):
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError:
+        return None
